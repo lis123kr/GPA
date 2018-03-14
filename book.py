56725e8cafb16706cdee9cd82520733257ff20e8
lis123kr <lis123kr@naver.com>
2018-03-14 18:25:29 +0900

    180314 book refactoring

--- book.py
+++ book.py
@@ -26,6 +26,7 @@ class Book:
 
 	col_ORF = None
 
+	col_basepair = ["A","G","C","T"]
 	# data list of each columns
 	GenomeStructure = []
 	
@@ -35,13 +36,14 @@ class Book:
 
 	NCR = []
 
-
 	# variables for preprocessing & analyzing
 	BPRaw = []
 
-	BP = []
+	BPRawLength = [] #
 
-	BPxSum = []
+	BP35 = []
+
+	BPxSum = [] #
 
 	BPxMajor = []
 	
@@ -51,7 +53,7 @@ class Book:
 
 	P0 = None
 
-	P0_Raw35 = []
+	P0_Raw35 = [] #
 
 	def type_check(self, L):
 		for i in range(0,len(L)):
@@ -61,18 +63,35 @@ class Book:
 				L[i] = str(L[i])
 		return L
 
-
 	def initialize(self):
 		self.filename = self.filename.split('.')[0]
-
+		self.nsheets = len(self.sheet_list)
 		self.GenomeStructure = self.type_check(self.GenomeStructure)
 		self.RepeatRegion = self.type_check(self.RepeatRegion)
 		self.ORF = self.type_check(self.ORF)
 		self.NCR = self.type_check(self.NCR)
 
+	def mergesheets(self):
+		for x in self.sheet_list:
+			sheet = self.xls.parse(x)
+			if self.P0 is None:
+				self.P0 = sheet
+			else:
+				self.P0 = pd.merge(self.P0, sheet, how='outer', on=[self.col_GenomeStructure, 
+					self.col_RepeatRegion, self.col_ORF, self.col_DumaPosition, self.col_DumaSeq])
+			sheet = sheet[ sheet[self.col_Sequence] != '-' ]
+			self.BPRaw.append(sheet)
+			self.BPRawLength.append(len(sheet))
 
-
-
-
-
-	
+	def get_major_minor(self, Passage):	
+		"""	
+			- major : 염기 A, G, C, T 중 가장 큰 값
+			- minor : 염기 중 두번째로 큰 값, argsort 후 인덱스 2의 값을 추출
+		"""
+		ranked_df = Passage.values.argsort(axis=1)		
+		arr = np.zeros((len(Passage),1))
+		for i, d in enumerate(ranked_df[:,2]):
+			arr[i] = Passage[i:i+1][self.col_basepair[d]]
+		major = pd.DataFrame(Passage.max(axis=1))
+		minor = pd.DataFrame(arr, index=major.index)		
+		return major, minor

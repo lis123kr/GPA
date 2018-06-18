efcb5f92025fa668f9409ece256dc09523a15d49
lis123kr <lis123kr@naver.com>
2018-06-18 17:08:40 +0900

    180618

---
+++ test.py
@@ -0,0 +1,21 @@
+import pickle as pk
+from book import Book
+from analyzer import Analyzer
+# 0 : gps / 1 : 증감
+flag = 0
+
+pfile = 'N_pOB.pkl'
+
+# input_files= [ 'genome.txt', 'repeat.txt', 'ORF.txt', 'NCR.txt']
+# dir_ = '.\\test\\'
+# inputs = {}
+# for file in input_files:
+# 	key = file.split('.')[0]
+# 	file_ = dir_ + file
+# 	with open(file_, 'r') as f:
+# 		inputs[key] = f.read().replace('\n',',').strip(',').split(',')
+
+book = Book.load_data(pfile)
+Analyze_type = 'Full' #"Difference_of_Minor"
+excel = Analyzer()
+excel.Analyze(Analyze_type, book)
